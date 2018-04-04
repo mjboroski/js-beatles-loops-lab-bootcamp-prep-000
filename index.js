7702ae2c4599fe629d5fd300ac38f747a1546625
Michael Boroski <mjboroski@gmail.com>
2018-04-04 21:45:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments){
   var array = {};
-  for (var i=0;i<musicians.length;i++){
+  for each i in musicians{
     array[i]=musicians[i]+" plays "+instruments[i];
   }
   return array;
